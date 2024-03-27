76f5a6a47ba574546de140d2e7f30f2097d07153
espen-servicewell <espen.lever@servicewell.se>
2024-03-27 14:09:31 +0100

    protobuf serialization

--- src/Akka.CQRS/Serialization/TradeEventSerializer.cs
+++ src/Akka.CQRS/Serialization/TradeEventSerializer.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -280,4 +280,3 @@ namespace Akka.CQRS.Serialization
         }
     }
 }
-*/
